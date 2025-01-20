6110eef0ebc9aaf432265b8c481e8bf4692b4ee4
claudio-cordova-estrada <cl.cordova@duocuc.cl>
2025-01-20 19:53:32 -0300

    formulario pago actualizado creacion pago
    creacion pago actualizado, aunque aun no toma las id de la sesión loggeada, porque aún no hay sesión loggeada asoefuiljasoefij

--- src/app/pages/formulario-pago/formulario-pago.page.ts
+++ src/app/pages/formulario-pago/formulario-pago.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormularioPago } from '../../interfaces/formulario-pago';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { SupabaseService } from 'src/app/supabase.service';
 
 @Component({
   selector: 'app-formulario-pago',
@@ -11,19 +12,26 @@ import { Router } from '@angular/router';
 })
 export class FormularioPagoPage implements OnInit {
 
-  constructor(private alertController: AlertController, private router: Router) { }
+  constructor(private alertController: AlertController, 
+    private router: Router,
+    private servicio:SupabaseService
+  ) { }
 
   message: string='';
   header: string='';
+  fecha: Date = new Date(Date.now());
+
   FP:FormularioPago = {
     usuario: '',
     montoPagado: 0,
     metodoPago: '',
     comentarios: '',
-    fecha: new Date(),
+    fecha: new Date(Date.now()),
   }
 
   ngOnInit() {
+    console.log(this.FP + ' | ' + this.FP.metodoPago + ' | ' + 
+      this.FP.montoPagado + ' | ' + this.FP.fecha);
   }
 
   public alertButtons = [
@@ -48,9 +56,30 @@ export class FormularioPagoPage implements OnInit {
     await alert.present();
   }
 
+  async presentAlertDenegado(header:string, message:string) {
+    const alert = await this.alertController.create({
+      header: this.header,
+      message: this.message,
+      buttons: [
+        {
+          text: 'Confirmar',
+          role: 'confirm',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
   onSubmit() {
-    console.log(this.FP);
+    console.log(this.FP)
+    console.log(this.FP.metodoPago + ' | ' + 
+      this.FP.montoPagado + ' | ' + this.FP.fecha);
+
     if(this.FP.metodoPago!=="" && this.FP.montoPagado>0){
+      this.pago(this.FP.montoPagado, this.FP.metodoPago, 
+        this.FP.comentarios);
+
       console.log("Formulario OK");
       this.header = 'Pago enviado con éxito'
       this.message = 'El formulario de pago será revisado por un administrador en la brevedad'
@@ -59,8 +88,15 @@ export class FormularioPagoPage implements OnInit {
     else{
       this.header = 'Error con el formulario'
       this.message = 'Uno de los campos fue rellenado de manera incorrecta, intentar nuevamente'
-      this.presentAlert(this.header, this.message);
+      this.presentAlertDenegado(this.header, this.message);
     }
+  }
+
+  pago(montoPagado:number, metodoPago:string, comentarios?:string){
+    console.log(this.FP.fecha);
+    this.FP.fecha = new Date(Date.now());
+    console.log(this.FP.fecha);
+    this.servicio.crearPago(montoPagado, metodoPago, this.FP.fecha, 1, 1, comentarios)
   }
 
 }
